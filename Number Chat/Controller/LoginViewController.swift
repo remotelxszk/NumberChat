1e7e7dde619010bd7504a81e457d1ef8adc3b0a4
remotelxszk <dominic.les.work@icloud.com>
2021-01-03 19:42:46 +0100

    Added a UISlider and linked it, changed some texts on the welcome/login screen, renamed UILabel from signInUserNumber to signInChatNumber

--- Number Chat/Controller/LoginViewController.swift
+++ Number Chat/Controller/LoginViewController.swift
@@ -11,12 +11,14 @@ import Firebase
 
 class LoginViewController: UIViewController {
 
-    @IBOutlet weak var signInUserNumber: UILabel!
+    @IBOutlet weak var signInChatNumber: UILabel!
     @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var chatSlider: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         loginButton.layer.cornerRadius = 10.0
+        chatSlider.value = 1
     }
 
     @IBAction func loginButtonPressed(_ sender: UIButton) {
