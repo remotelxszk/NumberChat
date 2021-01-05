64537bd9ca0a7f1df81c333566c1cd520f3dd2f0
remotelxszk <dominic.les.work@icloud.com>
2021-01-05 23:58:14 +0100

    Loading messages and fixed registering xib file

--- Number Chat/Controller/ChatViewController.swift
+++ Number Chat/Controller/ChatViewController.swift
@@ -30,7 +30,9 @@ class ChatViewController: UIViewController {
         title = "Chat \(currentChat!)"
         
         // Register Custom xib file
-        messageTableView.register(UINib(nibName: "TextMessage", bundle: nil), forCellReuseIdentifier: Constants.cellIdentifier)
+        messageTableView.register(UINib(nibName: "ChatCell", bundle: nil), forCellReuseIdentifier: Constants.cellIdentifier)
+        
+        loadMessages()
         
     }
 
@@ -92,6 +94,7 @@ extension ChatViewController: UITableViewDataSource {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: Constants.cellIdentifier, for: indexPath) as! ChatCell
         cell.textBody.text = message.body
+        cell.textSenderID.text = message.id
         
         // This is a message from the current user
         if message.id == Auth.auth().currentUser?.uid {
@@ -103,4 +106,50 @@ extension ChatViewController: UITableViewDataSource {
     }
     
     
+}
+
+//MARK: - LoadMessages
+
+extension ChatViewController {
+    func loadMessages() {
+        
+        // Download documents from this collectionName order by time sent
+        // and listen in real time for changes
+        db.collection(Constants.FireStore.collection + "\(currentChat!)")
+            .order(by: Constants.FireStore.dateField)
+            .addSnapshotListener { (querySnapshot, error) in
+            // Print an error if there is one
+            if let e = error {
+                print("There was an issue retrieving data from Firestore. \(e)")
+            } else {
+                // If there are no errors get documents
+                if let snapshotDocuments = querySnapshot?.documents {
+                    
+                    // Empty array
+                    self.messages = []
+                    
+                    // For every document in documents
+                    for document in snapshotDocuments {
+                        // Get document data
+                        let data = document.data()
+                        // If document data is not empty pass both as a string to these variables
+                        if let messageSender = data[Constants.FireStore.senderID] as? String, let messageBody = data[Constants.FireStore.bodyField] as? String {
+                            
+                            // Create a new message and add it to the message array
+                            let newMessage = TextMessage(id: messageSender, body: messageBody)
+                            self.messages.append(newMessage)
+                            
+                            // Reload TableView data
+                            DispatchQueue.main.async {
+                                self.messageTableView.reloadData()
+                                
+                                let indexPath = IndexPath(row: (self.messages.count - 1), section: 0)
+                                self.messageTableView.scrollToRow(at: indexPath, at: .top, animated: true)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
